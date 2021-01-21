fb3f10b3f1bd6fa65c572d638d1982916c55fe0a
DESKTOP-BJAL2D9\jsekulska <joanna.sekulska-nalewajko@p.lodz.pl>
2021-01-21 22:29:52 +0100

    Zmiana03_metoda_Policz

---
+++ klasa1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jsekulska on 21.01.2021.
+//
+
+#include "klasa1.h"
---
+++ klasa1.h
@@ -0,0 +1,14 @@
+//
+// Created by jsekulska on 21.01.2021.
+//
+
+#ifndef UNTITLED_KLASA1_H
+#define UNTITLED_KLASA1_H
+
+
+class klasa1 {
+
+};
+
+
+#endif //UNTITLED_KLASA1_H
